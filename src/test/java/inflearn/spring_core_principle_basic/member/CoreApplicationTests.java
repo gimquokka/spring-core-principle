50b0d3b7eae16ec369778045d5be41e132e6c095
gimquokka <gimquokka@gmail.com>
2021-07-21 09:30:49 +0900

    refactor: Remove dependency of test code
    Remove dependency of test code with AppConfig

--- src/test/java/inflearn/spring_core_principle_basic/member/CoreApplicationTests.java
+++ src/test/java/inflearn/spring_core_principle_basic/member/CoreApplicationTests.java
@@ -1,11 +1,19 @@
 package inflearn.spring_core_principle_basic.member;
 
+import inflearn.spring_core_principle_basic.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CoreApplicationTests {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
--- src/test/java/inflearn/spring_core_principle_basic/order/OrderServiceTest.java
+++ src/test/java/inflearn/spring_core_principle_basic/order/OrderServiceTest.java
@@ -1,16 +1,24 @@
 package inflearn.spring_core_principle_basic.order;
 
+import inflearn.spring_core_principle_basic.AppConfig;
 import inflearn.spring_core_principle_basic.member.Grade;
 import inflearn.spring_core_principle_basic.member.Member;
 import inflearn.spring_core_principle_basic.member.MemberService;
-import inflearn.spring_core_principle_basic.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
