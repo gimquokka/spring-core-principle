4815742d59fec3f54040cdc3c05b2759f824f5e7
gimquokka <gimquokka@gmail.com>
2021-07-21 10:58:34 +0900

    refactor: Replace AppConfig with Spring ApplicationContext
    Replace POJO AppConfig with Spring Framework

--- src/main/java/inflearn/spring_core_principle_basic/AppConfig.java
+++ src/main/java/inflearn/spring_core_principle_basic/AppConfig.java
@@ -8,23 +8,31 @@ import inflearn.spring_core_principle_basic.member.MemberServiceImpl;
 import inflearn.spring_core_principle_basic.member.MemoryMemberRepository;
 import inflearn.spring_core_principle_basic.order.OrderService;
 import inflearn.spring_core_principle_basic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
     }
 
 }
--- src/main/java/inflearn/spring_core_principle_basic/OrderApp.java
+++ src/main/java/inflearn/spring_core_principle_basic/OrderApp.java
@@ -5,19 +5,21 @@ import inflearn.spring_core_principle_basic.member.Member;
 import inflearn.spring_core_principle_basic.member.MemberService;
 import inflearn.spring_core_principle_basic.order.Order;
 import inflearn.spring_core_principle_basic.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "김진영", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "떡볶이", 3000);
+        Order order = orderService.createOrder(memberId, "떡볶이", 20000);
 
         System.out.println(order);
 
